d4c22e9406a7639fbb9aca6e143dd2d62fbcf4db
Chaya9 <chaya.pr@gmail.com>
2016-08-13 18:24:47 +0530

    ok1

--- src/mypackage2/Demo456.java
+++ src/mypackage2/Demo456.java
@@ -5,14 +5,25 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
 public class Demo456 {
 	@Test
 	public void testA(){
+		ExtentReports e = new ExtentReports("./ExtentReport.html");
+		ExtentTest v = e.startTest("hi");
+		v.log(LogStatus.PASS, "hi");
+		e.endTest(v);
+		e.flush();
+		
 		WebDriver driver = new FirefoxDriver();
 		driver.get("https://www.google.co.in/");
 		Reporter.log("test123", true);
 		Reporter.log("testing", true);
 		driver.close();
+		
 	}
 
 }
